544577b92b30d8fe651efa10baa0a26c9d2cc698
sizop <110180786+sizop@users.noreply.github.com>
2022-10-17 10:21:43 +0500

    Add files via upload
    Проверка даты и рег. номера

---
+++ check.py
@@ -0,0 +1,18 @@
+import re
+from datetime import date
+
+
+def data_change(y, m, d):
+    try:
+        date(y, m, d)
+        return True
+    except:
+        return False
+
+
+def reg_num_change(reg_num):
+    s_num = str(reg_num)
+
+    return 10 > len(s_num) > 7 and not re.search('[a-zA-Z]', s_num) and s_num[:1].isalpha() and s_num[1:4].isdigit() \
+           and s_num[4:6].isalpha() and s_num[6:].isdigit()
+
