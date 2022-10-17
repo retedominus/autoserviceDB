7fc6cee0c1ae8010dc78156a2f070d5f97cb798a
sizop <110180786+sizop@users.noreply.github.com>
2022-10-17 13:23:00 +0500

    Update check.py
    Поправил фильтр букв в гос. номере

--- check.py
+++ check.py
@@ -13,6 +13,6 @@ def data_change(y, m, d):
 def reg_num_change(reg_num):
     s_num = str(reg_num)
 
-    return 10 > len(s_num) > 7 and not re.search('[a-zA-Z]', s_num) and s_num[:1].isalpha() and s_num[1:4].isdigit() \
-           and s_num[4:6].isalpha() and s_num[6:].isdigit()
+    return 10 > len(s_num) > 7 and not re.search('[a-zA-Zйцгшщзфыплджэячьъбю]', s_num) and s_num[:1].isalpha()\
+           and s_num[1:4].isdigit() and s_num[4:6].isalpha() and s_num[6:].isdigit()
 
